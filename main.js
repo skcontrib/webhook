2809ef946bc1829652f2af8815e676bac2a7ee3d
skcontrib <61639131+skcontrib@users.noreply.github.com>
2020-07-13 21:14:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /*   
   Project by Zua 
+  Edited by skcontrib
   https://github.com/thatziv/webhook 
+  https://github.com/skcontrib
 */
 $(function(){
     $('#btn').click(function(){
@@ -13,7 +15,24 @@ $(function(){
             alert("Please Fill Out All The Fields");
             return false;
         }
-        $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
-
+        $.post(link, 
+            {"content": content, "username": username, "avatar_url": avatar}, );
+        
+    });
+    $("#content").keypress(function (e) {
+        if(e.which == 13 && !e.shiftKey) {
+            var link = $('#link').val();
+            var username = $('#username').val();
+            var content = $('#content').val();
+            var avatar = $('#avatar').val();
+            if (link==null || link=="",content==null || content=="")
+            {
+                alert("Please Fill Out All The Fields");
+                return false;
+            }
+            $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
+            $('#content').val('');
+            e.preventDefault();
+        };
     });
 });
